7d86f442d63c99cadde0c6c443df58db35283eac
Daniel Erenrich <daniel@erenrich.net>
2023-08-31 04:17:24 +0000

    add cd

--- build_toolforge.sh
+++ build_toolforge.sh
@@ -4,6 +4,7 @@
 # webservice --backend=kubernetes python3.11 shell
 
 source $HOME/www/python/venv/bin/activate
+cd ~/quonter-vandal
 poetry export --without-hashes -f requirements.txt -o requirements.txt
 pip install --no-cache-dir -r requirements.txt
 pip install ~/quonter-vandal
